d06e3755b12663473e68c0b75ccac54e254e7e1a
Zandro Gene <kyahandoy@gmail.com>
2024-07-18 14:10:31 +0800

    Kotlin Basics

--- src/Main.kt
+++ src/Main.kt
@@ -1,4 +1,24 @@
 
 fun main() {
+    val firstUserEmailId = "user_one@gmail.com"
 
+    println(displayAlertMessage(emailId = firstUserEmailId))
+    println()
+
+    val secondUserOperatingSystem = "Windows"
+    val secondUserEmailId = "user_two@gmail.com"
+
+    println(displayAlertMessage(secondUserOperatingSystem, secondUserEmailId))
+    println()
+
+    val thirdUserOperatingSystem = "Mac OS"
+    val thirdUserEmailId = "user_three@gmail.com"
+
+    println(displayAlertMessage(thirdUserOperatingSystem, thirdUserEmailId))
+    println()
+}
+
+
+fun displayAlertMessage(operatingSystem: String = "unknown", emailId: String): String {
+    return "There's a new sign-in request on $operatingSystem OS for your Google Account $emailId."
 }
