f8a46cbd57e12be34700c92cc4d8c1f2907baf76
Zandro Gene <kyahandoy@gmail.com>
2024-07-18 14:34:18 +0800

    Comparing Two Numbers

--- src/Main.kt
+++ src/Main.kt
@@ -16,9 +16,18 @@ fun main() {
 
     println(displayAlertMessage(thirdUserOperatingSystem, thirdUserEmailId))
     println()
+
+
+    println(trackScreenTime(300, 250))
+    println(trackScreenTime(300, 300))
+    println(trackScreenTime(200, 220))
 }
 
 
 fun displayAlertMessage(operatingSystem: String = "unknown", emailId: String): String {
     return "There's a new sign-in request on $operatingSystem OS for your Google Account $emailId."
+}
+
+fun trackScreenTime(timeSpentToday: Int, timeSpentYesterday: Int): Boolean {
+    return timeSpentToday > timeSpentYesterday
 }
