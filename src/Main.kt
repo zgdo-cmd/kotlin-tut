032634590d230a8d8819c38d6449ecec494e3745
Zandro Gene <kyahandoy@gmail.com>
2024-07-18 14:48:39 +0800

    Updating Main.kt

--- src/Main.kt
+++ src/Main.kt
@@ -18,9 +18,9 @@ fun main() {
     println()
 
 
-//    println(trackScreen(300, 250))
-//    println(trackScreen(300, 300))
-//    println(trackScreen(200, 220))
+    println(trackScreen(300, 250))
+    println(trackScreen(300, 300))
+    println(trackScreen(200, 220))
 }
 
 fun trackScreen(timeSpentToday: Int, timeSpentYesterday: Int): Boolean {
