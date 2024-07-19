51ef0a23fbe59f1512d092fb2b14e4678bebc0d3
Zandro Gene <kyahandoy@gmail.com>
2024-07-19 10:41:35 +0800

    Nullable Types and Safe Call Operator

--- src/Nullability.kt
+++ src/Nullability.kt
@@ -1,7 +1,20 @@
 fun main() {
 
     //Non-Nullable Types
-    var favoriteActor = "Sarah Oh"
+    var favoriteActor1: String = "Sarah Oh"
     // favoriteActor = null // This will cause a compilation error
-    println(favoriteActor.length)
+    println(favoriteActor1.length)
+
+    //Nullable Types
+    var favoriteActor2: String? = "Sarah Oh"
+    //println(favoriteActor2.length) //No safe call will cause and error
+    //Allowed
+    favoriteActor2 = null
+    println(favoriteActor2)
+
+    // Prints: null if nullable is null, otherwise prints the length of the string
+    var favoriteActor3: String? = "Sarah Oh"
+    //Accessing string property with safe call operator
+    println(favoriteActor3?.length)
+
 }
