c5be33acaeda75871acc09441d0444f983a7d354
zgdo <kyahandoy@gmail.com>
2024-07-20 11:18:43 -0700

    Classes&Objects

--- src/Classes&Objects.kt
+++ src/Classes&Objects.kt
@@ -1,3 +1,25 @@
 fun main() {
+    val SmartDevice = SmartDevice()
+
+    println("Device type is: ${SmartDevice.deviceType}")
+    SmartDevice.turnOn()
+    SmartDevice.turnOff()
+}
+
+class SmartDevice() {
+    val deviceType = "SmartTV"
+    val category = "Entertainment"
+    var deviceStatus = "online"
+
+
+    fun turnOn() {
+        println("Smart device is turned on.")
+    }
+
+    fun turnOff() {
+        println("Smart device is turned off.")
+    }
+
+
 
 }
