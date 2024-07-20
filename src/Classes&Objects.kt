35a94d9a5027bb1032f6ed11970b9676a6bc6756
Zandro Gene <kyahandoy@gmail.com>
2024-07-21 02:22:02 +0800

    Device Category

--- src/Classes&Objects.kt
+++ src/Classes&Objects.kt
@@ -2,6 +2,7 @@ fun main() {
     val SmartDevice = SmartDevice()
 
     println("Device type is: ${SmartDevice.deviceType}")
+    println("The Device is for: ${SmartDevice.category}")
     SmartDevice.turnOn()
     SmartDevice.turnOff()
 }
