1a5fb2a621714ce4b61061703042680605a0cd9b
Zandro Gene <kyahandoy@gmail.com>
2024-07-21 11:38:48 +0800

    Primary Constructors with Secondary Constructor

--- src/Classes&Objects.kt
+++ src/Classes&Objects.kt
@@ -1,22 +1,33 @@
 fun main() {
-    val SmartDevice = SmartDevice(deviceType = "Smart TV", category = "Entertainment")
+//    val SmartDevice = SmartDevice(deviceType = "Smart TV", category = "Entertainment")
+    val SmartDevice = SmartDevice(deviceType = "Smart TV", category = "Entertainment", 2)
 
-    println("Device type is: ${SmartDevice.deviceType}")
-    println("The Device is for: ${SmartDevice.category}")
-    SmartDevice.turnOn()
-    SmartDevice.turnOff()
+//    println("Device type is: ${SmartDevice.deviceType}")
+//    println("The Device is for: ${SmartDevice.category}")
+
 }
 
 class SmartDevice(val deviceType: String, val category: String) {
 
     var deviceStatus = "online"
 
+    constructor(deviceType: String, category: String, statusCode: Int) : this(deviceType, category) {
+        deviceStatus = when(statusCode) {
+            0 -> "online"
+            1 -> "offline"
+            else -> "unknown"
+        }
+
+    }
+
     fun turnOn() {
-        println("Smart device is turned on.")
+        println("Smart device is online.")
     }
 
     fun turnOff() {
-        println("Smart device is turned off.")
+        println("Smart device is turned offline.")
+        println(deviceStatus)
+
     }
 
 }
